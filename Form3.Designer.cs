da374e59f6fd2580f42ae150675da9cfe7469d99
Hafizh arrasyiid s <124231122+HafizhArrasyiidS@users.noreply.github.com>
2023-12-17 15:54:53 +0700

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace convert
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButtonG2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonG1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonDAG1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonHG1 = new System.Windows.Forms.RadioButton();
+            this.radioButtonKG2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonDAG2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonHG2 = new System.Windows.Forms.RadioButton();
+            this.radioButtonKG1 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(12, 6);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(562, 320);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(554, 294);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Konversi Massa";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(148, 154);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 28);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Konversi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // radioButtonG2
+            // 
+            this.radioButtonG2.AutoSize = true;
+            this.radioButtonG2.ForeColor = System.Drawing.Color.White;
+            this.radioButtonG2.Location = new System.Drawing.Point(205, 3);
+            this.radioButtonG2.Name = "radioButtonG2";
+            this.radioButtonG2.Size = new System.Drawing.Size(31, 17);
+            this.radioButtonG2.TabIndex = 18;
+            this.radioButtonG2.TabStop = true;
+            this.radioButtonG2.Text = "g";
+            this.radioButtonG2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonG1
+            // 
+            this.radioButtonG1.AutoSize = true;
+            this.radioButtonG1.ForeColor = System.Drawing.Color.White;
+            this.radioButtonG1.Location = new System.Drawing.Point(205, 2);
+            this.radioButtonG1.Name = "radioButtonG1";
+            this.radioButtonG1.Size = new System.Drawing.Size(31, 17);
+            this.radioButtonG1.TabIndex = 17;
+            this.radioButtonG1.TabStop = true;
+            this.radioButtonG1.Text = "g";
+            this.radioButtonG1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDAG1
+            // 
+            this.radioButtonDAG1.AutoSize = true;
+            this.radioButtonDAG1.ForeColor = System.Drawing.Color.White;
+            this.radioButtonDAG1.Location = new System.Drawing.Point(138, 3);
+            this.radioButtonDAG1.Name = "radioButtonDAG1";
+            this.radioButtonDAG1.Size = new System.Drawing.Size(43, 17);
+            this.radioButtonDAG1.TabIndex = 16;
+            this.radioButtonDAG1.TabStop = true;
+            this.radioButtonDAG1.Text = "dag";
+            this.radioButtonDAG1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonHG1
+            // 
+            this.radioButtonHG1.AutoSize = true;
+            this.radioButtonHG1.ForeColor = System.Drawing.Color.White;
+            this.radioButtonHG1.Location = new System.Drawing.Point(64, 3);
+            this.radioButtonHG1.Name = "radioButtonHG1";
+            this.radioButtonHG1.Size = new System.Drawing.Size(37, 17);
+            this.radioButtonHG1.TabIndex = 15;
+            this.radioButtonHG1.TabStop = true;
+            this.radioButtonHG1.Text = "hg";
+            this.radioButtonHG1.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonKG2
+            // 
+            this.radioButtonKG2.AutoSize = true;
+            this.radioButtonKG2.ForeColor = System.Drawing.Color.White;
+            this.radioButtonKG2.Location = new System.Drawing.Point(3, 3);
+            this.radioButtonKG2.Name = "radioButtonKG2";
+            this.radioButtonKG2.Size = new System.Drawing.Size(37, 17);
+            this.radioButtonKG2.TabIndex = 14;
+            this.radioButtonKG2.TabStop = true;
+            this.radioButtonKG2.Text = "kg";
+            this.radioButtonKG2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonDAG2
+            // 
+            this.radioButtonDAG2.AutoSize = true;
+            this.radioButtonDAG2.ForeColor = System.Drawing.Color.White;
+            this.radioButtonDAG2.Location = new System.Drawing.Point(137, 3);
+            this.radioButtonDAG2.Name = "radioButtonDAG2";
+            this.radioButtonDAG2.Size = new System.Drawing.Size(43, 17);
+            this.radioButtonDAG2.TabIndex = 13;
+            this.radioButtonDAG2.TabStop = true;
+            this.radioButtonDAG2.Text = "dag";
+            this.radioButtonDAG2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonHG2
+            // 
+            this.radioButtonHG2.AutoSize = true;
+            this.radioButtonHG2.ForeColor = System.Drawing.Color.White;
+            this.radioButtonHG2.Location = new System.Drawing.Point(64, 3);
+            this.radioButtonHG2.Name = "radioButtonHG2";
+            this.radioButtonHG2.Size = new System.Drawing.Size(37, 17);
+            this.radioButtonHG2.TabIndex = 12;
+            this.radioButtonHG2.TabStop = true;
+            this.radioButtonHG2.Text = "hg";
+            this.radioButtonHG2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonKG1
+            // 
+            this.radioButtonKG1.AutoSize = true;
+            this.radioButtonKG1.ForeColor = System.Drawing.Color.White;
+            this.radioButtonKG1.Location = new System.Drawing.Point(3, 3);
+            this.radioButtonKG1.Name = "radioButtonKG1";
+            this.radioButtonKG1.Size = new System.Drawing.Size(37, 17);
+            this.radioButtonKG1.TabIndex = 11;
+            this.radioButtonKG1.TabStop = true;
+            this.radioButtonKG1.Text = "kg";
+            this.radioButtonKG1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(147, 68);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.tabPage2.Controls.Add(this.button5);
+            this.tabPage2.Controls.Add(this.button4);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.button2);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.textBox3);
+            this.tabPage2.Controls.Add(this.textBox2);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(554, 294);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Konversi Suhu";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButtonKG1);
+            this.panel1.Controls.Add(this.radioButtonHG1);
+            this.panel1.Controls.Add(this.radioButtonDAG1);
+            this.panel1.Controls.Add(this.radioButtonG1);
+            this.panel1.Location = new System.Drawing.Point(147, 92);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(245, 23);
+            this.panel1.TabIndex = 20;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.radioButtonKG2);
+            this.panel2.Controls.Add(this.radioButtonHG2);
+            this.panel2.Controls.Add(this.radioButtonDAG2);
+            this.panel2.Controls.Add(this.radioButtonG2);
+            this.panel2.Location = new System.Drawing.Point(147, 125);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(242, 23);
+            this.panel2.TabIndex = 21;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(23, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input (Celcius)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(23, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Reamur";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(23, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Farenheit";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(148, 32);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(148, 109);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(148, 70);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(348, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 39);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Konversi";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(133, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 30);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Pemanas";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(370, 204);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 30);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Pendingin";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(235, 249);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(109, 39);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Ulang";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.ClientSize = new System.Drawing.Size(586, 338);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form3";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButtonG2;
+        private System.Windows.Forms.RadioButton radioButtonG1;
+        private System.Windows.Forms.RadioButton radioButtonDAG1;
+        private System.Windows.Forms.RadioButton radioButtonHG1;
+        private System.Windows.Forms.RadioButton radioButtonKG2;
+        private System.Windows.Forms.RadioButton radioButtonDAG2;
+        private System.Windows.Forms.RadioButton radioButtonHG2;
+        private System.Windows.Forms.RadioButton radioButtonKG1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace convert
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int angka = int.Parse(textBox1.Text);
+
+            var hasil = 0;
+            if (radioButtonKG1.Checked)
+            {
+                if (radioButtonKG2.Checked)
+                {
+                    hasil = angka * 1;
+                }
+                else if (radioButtonHG2.Checked)
+                {
+                    hasil = angka * 10;
+                }
+                else if (radioButtonDAG2.Checked)
+                {
+                    hasil = angka * 100;
+                }
+                else if (radioButtonG2.Checked)
+                {
+                    hasil = angka * 1000;
+                }
+            }
+
+            if (radioButtonHG1.Checked)
+            {
+                if (radioButtonKG2.Checked) 
+                {
+                    hasil = angka / 1;
+                }
+                else if (radioButtonHG2.Checked)
+                {
+                    hasil = angka * 1;
+                }
+                else if (radioButtonDAG2.Checked)
+                {
+                   hasil = angka * 10;
+                }
+                else if (radioButtonG2.Checked)
+                {
+                    hasil = angka * 100;
+                }
+            }
+
+            if (radioButtonDAG1.Checked)
+            {
+               if (radioButtonKG2.Checked )
+                {
+                    hasil = angka / 100;
+                }
+               else if (radioButtonHG2.Checked)
+                {
+                    hasil = angka / 10;
+                }
+               else if (radioButtonDAG2.Checked)
+                {
+                    hasil = angka * 1;
+                }
+               else if (radioButtonG2.Checked )
+                {
+                    hasil = angka * 10;
+                }
+            }
+
+            if (radioButtonG1.Checked)
+            {
+                if (radioButtonKG2.Checked)
+                {
+                    hasil = angka / 1000;
+                }
+                else if (radioButtonHG2.Checked)
+                {
+                    hasil = angka / 100;
+                }
+                else if (radioButtonDAG2.Checked)
+                {
+                    hasil = angka / 10;
+                }
+                else if (radioButtonG2.Checked)
+                {
+                    hasil = angka / 1;
+                }
+            }
+            
+            
+            MessageBox.Show("Hasil = " + hasil);
+        }
+    }
+}
